946223b87eaf936af65855a577c88a5a7fcf061e
Tristan <kraig.tri125@gmail.com>
2014-01-30 18:55:03 -0500

    Mot implementer. Liste en debut

--- Mot.py
+++ Mot.py
@@ -2,21 +2,52 @@
 import random
 
 class Mot(object):
-	
+
 	def __init__(self, cle = None):
-		# TODO
+				if ( (cle == None) or (not cle)):
+						self.cle = self.generate_RndCompte()
+				else:
+						self.cle = cle
+				self.compte = 0
+
+	def generate_RndCompte(self):
+				length = random.randrange(2,15) #mot de 2 à 14 lettres 
+				alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+				list_mot = []
+				for x in range(length):
+					lettre = random.randrange(len(alphabet))
+					list_mot.append(alphabet[lettre])
+				return ''.join(list_mot)
+				
 	def __eq__(self, other):
-		# TODO
+		if isinstance(other, self.__class__):
+			return self.cle == other.cle
+		else:
+			return False
+			
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
 	def __lt__(self, other):
-		# TODO
+		if not isinstance(other, self.__class__):
+			return NotImplemented
+		return self.cle < other.cle
+	
+	def __gt__(self, other):
+		if not isinstance(other, self.__class__):
+			return NotImplemented
+		return self.cle > other.cle
+
+
 	def get_compte(self):
-		# TODO
+		return self.compte
+
 	def get_cle(self):
-		# TODO
+		return self.cle
+
 	def incrementer(self):
-		# TODO
+		self.compte+= 1
+
 	def decrementer(self):
-		# TODO
-
-
+		self.compte-= 1
 
